100fc0421e8646714fe50226f7552a895217e996
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 02:23:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ class Circle{
   get radius(){
     return this._radius;
   }
-  //
-  // get diameter(){
-  //   return this.radius * 2;
-  // }
-  //
-  // get circumference(){
-  //   return this.radius * 2 * Math.PI;
-  // }
-  //
-  // get area(){
-  //   return Math.PI * this.radius ** 2;
-  // }
+  
+  get diameter(){
+    return this._radius * 2;
+  }
+  
+  get circumference(){
+    return this._radius * 2 * Math.PI;
+  }
+  
+  get area(){
+    return Math.PI * this._radius ** 2;
+  }
 
-  // set radius(newRadius){
-  //   this.radius = newRadius;
-  // }
-  //
-  // set diameter(newDiameter){
-  //   this.radius = newDiameter / 2;
-  // }
-  //
-  // set circumference(newCircumference){
-  //   this.radius = newCircumference / Math.PI / 2 ;
-  // }
+  set radius(newRadius){
+    this._radius = newRadius;
+  }
+  
+  set diameter(newDiameter){
+    this._radius = newDiameter / 2;
+  }
+  
+  set circumference(newCircumference){
+    this._radius = newCircumference / Math.PI / 2 ;
+  }
 }
