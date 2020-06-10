c7fb443fc897c2c74770dc535a47d26a8484d5da
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 02:19:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ class Circle{
     return Math.PI * this.radius ** 2;
   }
 
-  set radius(radius){
-    this.radius = radius;
+  set radius(newRadius){
+    this.radius = newRadius;
   }
 
-  set diameter(diameter){
-    this.radius = diameter / 2;
+  set diameter(newDiameter){
+    this.radius = newDiameter / 2;
   }
 
-  set circumference(circumference){
-    this.radius = circumference / Math.PI / 2 ;
+  set circumference(newCircumference){
+    this.radius = newCircumference / Math.PI / 2 ;
   }
 }
