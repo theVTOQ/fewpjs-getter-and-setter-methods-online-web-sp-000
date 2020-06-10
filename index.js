7a8f73d0255f9690e8d6e12bb57151e11d94c483
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 02:20:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Add your Circle class here
 class Circle{
-  constructor(radius){
-    this._radius = radius;
-  }
-
-  get radius(){
-    return this.radius;
-  }
-
-  get diameter(){
-    return this.radius * 2;
-  }
-
-  get circumference(){
-    return this.radius * 2 * Math.PI;
-  }
-
-  get area(){
-    return Math.PI * this.radius ** 2;
-  }
+  // constructor(radius){
+  //   this._radius = radius;
+  // }
+  //
+  // get radius(){
+  //   return this.radius;
+  // }
+  //
+  // get diameter(){
+  //   return this.radius * 2;
+  // }
+  //
+  // get circumference(){
+  //   return this.radius * 2 * Math.PI;
+  // }
+  //
+  // get area(){
+  //   return Math.PI * this.radius ** 2;
+  // }
 
   // set radius(newRadius){
   //   this.radius = newRadius;
